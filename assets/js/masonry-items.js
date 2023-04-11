34b9e69dd111e5fef31a1b63aee02281b03772de
Lorenzo Fiori <127093378+LoryFr@users.noreply.github.com>
2023-04-11 15:01:51 +0200

    Masonry Grid Color Fixes

--- assets/js/masonry-items.js
+++ assets/js/masonry-items.js
@@ -3,15 +3,15 @@
 const posts = []
 
 const images = [
-    'assets/img/stills/masonry1.png',
-    'assets/img/stills/masonry2.png',
-    'assets/img/stills/masonry3.jpg',
-    'assets/img/stills/masonry4.png',
-    'assets/img/stills/masonry5.png',
-    'assets/img/stills/masonry6.png',
-    'assets/img/stills/masonry7.png',
-    'assets/img/stills/masonry8.png',
-    'assets/img/stills/masonry9.png'
+    'assets/img/stills/masonry1.webp',
+    'assets/img/stills/masonry2.webp',
+    'assets/img/stills/masonry3.webp',
+    'assets/img/stills/masonry4.webp',
+    'assets/img/stills/masonry5.webp',
+    'assets/img/stills/masonry6.webp',
+    'assets/img/stills/masonry7.webp',
+    'assets/img/stills/masonry8.webp',
+    'assets/img/stills/masonry9.webp'
 ]
 
 let imageIndex = 0;
@@ -19,7 +19,7 @@ let imageIndex = 0;
 for(let i = 1; i <= 80; i++){
     let item = {
         id: i,
-        title: `Post ${i}`,
+        title: ``,
         date: `${i < 10 ? 0 : ''}${i}/10/2021 `,
         image: images[imageIndex]
     }
