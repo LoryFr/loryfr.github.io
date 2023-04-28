d40b969e08fd869262f47168280723734ff4621b
Lorenzo Fiori <127093378+LoryFr@users.noreply.github.com>
2023-04-28 10:22:50 +0200

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,14 +1,30 @@
 if (screen.width < 600) {
 
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.bottom = "0";
-    } else {
-        document.getElementById("navbar").style.bottom = "-5rem";
-    }
-    prevScrollpos = currentScrollPos;
-    }
+    // var prevScrollpos = window.pageYOffset;
+    // window.onscroll = function() {
+    // var currentScrollPos = window.pageYOffset;
+    // if (prevScrollpos > currentScrollPos) {
+    //     document.getElementById("navbar").style.bottom = "0";
+    // } else {
+    //     document.getElementById("navbar").style.bottom = "-5rem";
+    // }
+    // prevScrollpos = currentScrollPos;
+    // }
 
+    window.addEventListener("scroll", function(){
+
+let currentSCroll = document.documentElement.scrollTop;
+
+if(currentSCroll > 100) {
+    document.getElementById("navbar").style.bottom = "-5rem";
+
+
+} else {
+    document.getElementById("navbar").style.bottom = "0";
+
+     }
+
+
+    });
+ 
 }
