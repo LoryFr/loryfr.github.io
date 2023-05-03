56c45e5d03dc03ebe27cc6b00efcb09a3f9f7472
Lorenzo Fiori <127093378+LoryFr@users.noreply.github.com>
2023-05-03 12:00:30 +0200

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,10 +1,18 @@
 if (screen.width < 600) {
+
     window.addEventListener("scroll", function(){
+
 let currentSCroll = document.documentElement.scrollTop;
+
 if(currentSCroll > 100) {
+
     document.getElementById("navbar").style.bottom = "-5rem";
+
 } else {
+
     document.getElementById("navbar").style.bottom = "0";
+
      }
+     
     });
 }
