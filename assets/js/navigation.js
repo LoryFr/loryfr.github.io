a3d93274a31518d857fe11eee9b8b9c37cb2153f
Lorenzo Fiori <127093378+LoryFr@users.noreply.github.com>
2023-09-05 23:20:48 +0200

    Delete assets directory

--- assets/js/navigation.js
+++
@@ -1,9 +0,0 @@
-const NavigationToggle = document.querySelector('.nav-toggle')
-const NavigationMenu = document.querySelector('.nav-menu')
-const Content = document.querySelector('.content')
-
-NavigationToggle.addEventListener('click', () => {
-    NavigationToggle.classList.toggle('active')
-    NavigationMenu.classList.toggle('active')
-    Content.classList.toggle('active')
-})
